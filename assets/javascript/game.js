3b2938f6dc66a8630207e7e58fbdda601645432c
Dana Nobile <danagnobile@gmail.com>
2020-04-24 09:59:27 -0400

    Added crystal value random generator to reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 
 // Computer generate random number between 19-120
 // Radom number is displayed on screen at start of game
-// Make images interactive
+// Make images interactive with click
 // Assign random value to image buttons between 1-12
-// When player clicks button, value is added to score (counter)
+// When player clicks button, value is added to score 
 // Win => total score = random number displayed at beginning
 // Lose => total score > random number displayed at beginning  
 // Restart happens after win or lose
@@ -12,28 +12,8 @@
 // New random number
 // Images get new value
 // User total score counter start set to 0
-// Keeps count of total wins and loses throughout game until page is refreshed
+// Keeps count of total wins and loses throughout game until reset implemented
 
-
-
-// Notes
-
-//Function to generate number within range
-// function randomNumber(min, max) {
-//     return Math.floor(Math.random() * (max - min) + min);
-
-// // Call function within range for image value
-// document.write(randomNumber(1, 12));
-
-// // Call function within range for computerChoice
-// document.write(randomNumber(19, 120));
-
-
-// //Displays:
-// $("winCount").text(wins);
-// // $("loseCount").text(losses);
-// $("randomNumber").text(computerChoice);
-// $("score").text(totalScore);
 
 $(document).ready(function () {
 
@@ -60,24 +40,19 @@ $(document).ready(function () {
         console.log(crystal4);
     }
 
-
-
     $(function randomNumber() {
         // This generates COMPUTER Random Number:
         var randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
         $(".computerChoice").text(randomNumber);
         console.log(randomNumber);
-        // });
 
         startGame();
-
 
         // This adds crystal value to total score: 
         $("#crystal1").on("click", function () {
             totalScore = crystal1 + totalScore
             $(".scoreTally").html("<h4>" + totalScore + "</h4>")
             play()
-
         });
 
 
@@ -91,15 +66,12 @@ $(document).ready(function () {
             totalScore = crystal3 + totalScore
             $(".scoreTally").html("<h4>" + totalScore + "</h4>")
             play()
-
         });
 
         $("#crystal4").on("click", function () {
             totalScore = crystal4 + totalScore
             $(".scoreTally").html("<h4>" + totalScore + "</h4>")
             play()
-
-
         });
 
 
@@ -120,10 +92,10 @@ $(document).ready(function () {
                 losses++
                 $("#losses").text("Losses: " + losses);
                 console.log("loss");
-                reset()
+                reset();
+                startGame();
             }
         }
-
 
     });
 });
